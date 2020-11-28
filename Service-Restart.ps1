dc98b970bb69530f65995061cc291bdb59fae0a5
armaanPy <52916251+armaanPy@users.noreply.github.com>
2020-11-28 19:54:26 +0000

    Add files via upload

---
+++ Service-Restart.ps1
@@ -0,0 +1,32 @@
+﻿# Author: Armaan Hussain
+# Date: 27/11/2020
+# Description: Restarts the Spooler service.
+
+
+$date = (Get-Date -Format "ddMMyyyy")
+$time = (Get-Date -Format "HHmm")
+$time.ToUniversalTime()
+$logFile = "L:\Armaan\ScriptLocation\Service_Restart$date.txt"
+
+Clear-Host
+
+$srvName = "Spooler"
+$service = Get-Service -Name $srvName
+
+if ($service.status -eq "Stopped") {
+echo "$srvName is already stopped. Exiting…" >> $logFile
+Exit
+} 
+
+else{ ($service.status -eq "Running")
+"Current status: $srvName is now " + $service.status >> $logFile
+Stop-Service $srvName
+$serviceAfter = Get-Service -Name $srvName
+"$srvName is now " + $serviceAfter.status >> $logFile
+Start-Sleep -s 5
+echo "Waiting..." >> $logFile
+Start-Service $srvName
+$serviceAfterStart = Get-Service -Name $srvName
+"$srvName is now " + $serviceAfterStart.status >> $logFile
+echo "Service: Spooler has been restarted at $time UTC." >> $logFile
+}
